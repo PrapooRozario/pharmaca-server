c7b884d2f7ec2cb8713c0a17189fe1e22322c035
PrapooRozario <albart2022@gmail.com>
2025-01-18 22:03:39 +0600

    Created Payment Gateway API and Some Problems Fixed

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 app.use(express.json());
 app.use(cors());
+const stripe = require("stripe")(process.env.PAYMENT_SECRET_KEY);
 
 const uri = `mongodb+srv://albart2022:albart_2024@cluster0.qgpkx.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
@@ -39,6 +40,7 @@ async function run() {
     const usersCollection = client.db("Pharmaca").collection("Users");
     const cartsCollection = client.db("Pharmaca").collection("Carts");
     const categoryCollection = client.db("Pharmaca").collection("Categories");
+    const paymentsCollection = client.db("Pharmaca").collection("Payments");
 
     // Get All Products API
     app.get("/products", async (req, res) => {
@@ -91,7 +93,7 @@ async function run() {
       res.status(200).send(result);
     });
 
-    // Products Cart API
+    // Products Cart Add
     app.post("/products/carts", verifyToken, async (req, res) => {
       const cart = req?.body;
       if (req?.user?.email !== cart?.email)
@@ -109,7 +111,7 @@ async function run() {
           .send({ message: "Product already exists in your cart." });
       }
     });
-
+    //  User Product Carts with More Details
     app.get("/products/carts", verifyToken, async (req, res) => {
       const email = req?.user?.email;
       if (email !== req?.query?.email)
@@ -138,23 +140,38 @@ async function run() {
             },
           },
           {
+            $group: {
+              _id: null,
+              allProducts: {
+                $push: {
+                  productId: "$productDetails._id",
+                  quantity: "$quantity",
+                  itemName: "$productDetails.itemName",
+                  perUnitPrice: "$productDetails.perUnitPrice",
+                  discountPercentage: "$productDetails.discountPercentage",
+                  company: "$productDetails.company",
+                  _id: "$_id",
+                  totalPrice: "$totalPrice",
+                },
+              },
+              totalCartPrice: {
+                $sum: "$totalPrice",
+              },
+            },
+          },
+          {
             $project: {
-              username: 1,
-              email: 1,
-              productId: 1,
-              quantity: 1,
-              itemName: "$productDetails.itemName",
-              perUnitPrice: "$productDetails.perUnitPrice",
-              discountPercentage: "$productDetails.discountPercentage",
-              company: "$productDetails.company",
-              totalPrice: 1,
+              _id: 0,
+              totalCartPrice: 1,
+              allProducts: 1,
             },
           },
         ])
         .toArray();
-      res.status(200).json(result);
+      res.status(200).json(result[0]);
     });
 
+    //  Delete Cart
     app.delete("/products/carts/:id", verifyToken, async (req, res) => {
       const email = req?.user?.email;
       if (email !== req?.query?.email)
@@ -163,11 +180,13 @@ async function run() {
       const result = await cartsCollection.deleteOne({
         _id: new ObjectId(cartProductId),
       });
-
       res.status(200).send(result);
     });
 
-    app.put("/products/cart/:id", async (req, res) => {
+    //  Quantity Update
+    app.put("/products/cart/:id", verifyToken, async (req, res) => {
+      if (req?.user?.email !== req?.query?.email)
+        return res.status(403).send({ message: "Forbidden" });
       const { quantity } = req?.body;
       const result = await cartsCollection.updateOne(
         { _id: new ObjectId(req?.params?.id) },
@@ -176,6 +195,13 @@ async function run() {
         }
       );
 
+      res.status(200).send(result);
+    });
+    //  Delete User All Carts
+    app.delete("/products/carts", async (req, res) => {
+      const result = await cartsCollection.deleteMany({
+        email: req?.query?.email,
+      });
       res.status(200).send(result);
     });
 
@@ -208,6 +234,26 @@ async function run() {
       } else {
         res.status(400).send({ message: "User already exists" });
       }
+    });
+
+    app.post("/create-payment-intent", verifyToken, async (req, res) => {
+      const { amount } = req.body;
+      try {
+        const paymentIntent = await stripe.paymentIntents.create({
+          amount,
+          currency: "usd",
+          payment_method_types: ["card"],
+        });
+        res.status(200).send({ clientSecret: paymentIntent?.client_secret });
+      } catch {
+        res.status(500).send({ error: error.message });
+      }
+    });
+    // Payments API
+    app.post("/products/payments", verifyToken, async (req, res) => {
+      const paymentDetails = req?.body;
+      const result = await paymentsCollection.insertOne(paymentDetails);
+      res.status(201).send(result);
     });
 
     // JWT API
